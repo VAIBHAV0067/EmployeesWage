7fdf3a46d0e311936a52fd12b29d85d4540021b1
tutorial6 <manapure.vaibhav542@gmail.com>
2022-03-08 09:54:07 +0530

    Switch Case Statement

--- EmployeesWageCaculation/EmployeesWageCaculation/Program.cs
+++ EmployeesWageCaculation/EmployeesWageCaculation/Program.cs
@@ -3,11 +3,34 @@ namespace EmployeesWageCaculation
 {
     class Program
     {
+        public const int IS_FULL_TIME = 2;
+        public const int IS_PART_TIME = 1;
+        public const int Emp_Rate_Per_Hour = 20;
+        
         static void Main(string[] args)
         {
+            int empHrs = 0;
+            int empWage = 0;
 
-            Class1.CheckEmployesPresent();
+            Random random = new Random();
+            int empCheck = random.Next(2);
 
+            switch (empCheck)
+            {
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+            }
+
+            empWage = empHrs * Emp_Rate_Per_Hour;
+
+            Console.WriteLine("Emp Wage :" + empWage);
         }
     }
 }
